189663c6bce8a0d1a94020a9a9f105265d638db3
Victor Gutierrez Calderon <gtrrz.victor@gmail.com>
2022-07-02 10:55:39 +1000

    add scafolding

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.get( "/", ( req, res ) => {
+    res.send("first lines of code!")
+} );
+
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
