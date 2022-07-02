f728b8ac2ea88a6f2ad47620323fd0a23a02a902
Victor Gutierrez Calderon <gtrrz.victor@gmail.com>
2022-07-02 12:33:15 +1000

    test 1

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash
 
-#give permission for everything in the express-app directory
+#give permission for everything in the pp-api directory
 sudo chmod -R 777 /home/ec2-user/pp-api
 
 #navigate into our working directory where we have all our github files
@@ -12,6 +13,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-yarn install
-
-pm2 start dist/index.js
+npm install
+npm run build
+#start our node app in the background
+node dist/index.js > app.out.log 2> app.err.log < /dev/null & 
