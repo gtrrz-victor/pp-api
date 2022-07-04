1cf842b20584d920832a07186c7bb6293887a725
Victor Gutierrez Calderon <gtrrz.victor@gmail.com>
2022-07-04 16:44:15 +1000

    fix typo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,7 +7,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install v16
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+DIR="/home/ec2-user/pp-api"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
